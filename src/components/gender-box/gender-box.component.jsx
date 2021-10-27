50c3ce70c30daedc4091b5dcd3e79daa3138b2d7
Yurii <elidanvalacaus@gmail.com>
2021-10-27 16:02:09 +0300

    troubles with inputs and gender boxes

--- src/components/gender-box/gender-box.component.jsx
+++ src/components/gender-box/gender-box.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-export const GenderBox = ({icon, gender, alt}) => {
-    return <div className="gender-box">
-     <img src={icon} alt={alt}/>
-      <span>{gender}</span>
+import './gender-box.styles.scss';
+
+export const GenderBox = ({icon, gender, gendercontainer, handleGender, alt}) => {
+  
+    return <div gendercontainer={gendercontainer}
+    className={`${handleGender() ? 'gender-choosen' : ''} gender-box`}
+    onClick={handleGender}
+    >
+     <img src={icon} alt={alt} className='gender-icon'/>
+      <span className='gender-text'>{gender}</span>
     </div>
 }
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -4,17 +4,39 @@ import topIcon from '../../assets/topIcon.svg';
 import inputIcon from '../../assets/inputIcon.svg';
 import { GenderBox } from '../gender-box/gender-box.component';
 import male from '../../assets/male1.svg';
+import maleClicked from '../../assets/male1Clicked.svg';
 import female from '../../assets/female1.svg';
+import femaleClicked from '../../assets/female1clicked.svg';
 import other from '../../assets/other.svg';
 
 
 export const SignUp = () => {
   
+  const [gender, setGender] = useState(' ')
   const [email, setEmail] = useState(' ');
   const [pwd, setPwd] = useState('');
   const [pwdConfirm, setPwdConfirm] = useState('');
   const [typePwd, setTypePwd] = useState('password');
   const [typePwdConfirm, setTypePwdConfirm] = useState('password');
+  const [newMaleIcon, setNewMaleIcon] = useState(male);
+
+const handleGenderMale = () => {
+  console.log(gender, 'hello');
+  setGender('male');
+ 
+}
+
+const handleMaleColor = () => {
+  setNewMaleIcon(maleClicked);
+}
+
+const handleGenderFemale = () => {
+  setGender('female')
+}
+
+const handleGenderOther = () => {
+  setGender('other')
+}
 
   const handleEmail = (e) => {
       setEmail(e.target.value);
@@ -28,19 +50,6 @@ const handlePwdConfirm = (e) => {
     setPwdConfirm(e.target.value);
 }
 
-const isEmailValid = () => {
-    const check = /\S+@\S+\.\S+/;
-    return check.test(email.toLowerCase())
-};
-const isPwdValid = () => {
-    const check = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    return check.test(pwd);
-};
-
-const isPwdConfirmValid = () => {
-   return pwd === pwdConfirm ? true : false
-};
-
 const showPwd = () => {
   typePwd === 'password' ? 
   setTypePwd('text') :
@@ -52,27 +61,73 @@ const showPwdConfirm = () => {
   setTypePwdConfirm('text') :
   setTypePwdConfirm('password')
 }
+
+const isEmailValid = () => {
+  const check = /\S+@\S+\.\S+/;
+  return check.test(email.toLowerCase())
+};
+const isPwdValid = () => {
+  const check = /[a-zA-Z0-9]{6,50}/;
+  return check.test(pwd);
+};
+
+const isPwdConfirmValid = () => {
+ return pwd === pwdConfirm ? true : false
+};
+
+const showAlert = () => {
+  if(isEmailValid() && isPwdValid() && isPwdConfirmValid()){
+    return alert(`
+    You signed up!
+    Your gender: ${gender}
+    Your email: ${email}
+    Your password: ${pwd}
+    `)
+  } else if (!isEmailValid() || !isPwdValid() || !isPwdConfirmValid()){
+    return alert(`
+    Please fill input fields correct or/and gender selection.
+    `)
+  }
+}
   
     return (
     <div className='sign-up-page-wrapper'>
       <form className='form-container'>
         <div className='form-header'>
-          <img src={topIcon} alt="top-icon"/>
+          <img src={topIcon} alt="top-icon" className='top-icon'/>
           <h1 className='main-title'>
            Sign Up with email
           </h1>
         </div>
         <div className='gender-container'>
-        <h2 >Gender</h2>
+        <h2 className='secondary-title'
+        style={{marginLeft: '0'}}>
+        Gender</h2>
         <div className="gender-boxes-wrapper">
-         <GenderBox icon={male} gender={"Male"} alt='male'/>
-         <GenderBox icon={female} gender={"Female"} alt='female'/>
-         <GenderBox icon={other} gender={"Other"} alt='other'/>
+         <GenderBox icon={handleMaleColor ? maleClicked : newMaleIcon} 
+         gender={"Male"} 
+         alt='male'
+         value={gender} 
+         handleGender={handleGenderMale}
+         onMouseDown={handleMaleColor}/>
+
+         <GenderBox icon={female} 
+         gender={"Female"}
+          alt='female'
+         value={gender} 
+         handleGender={handleGenderFemale}/>
+
+         <GenderBox icon={other} 
+         gender={"Other"} 
+         alt='other'
+         value={gender} 
+         handleGender={handleGenderOther}/>
         </div>
         </div>
 
         <div className='inputs-container'>
           <div className='input-wrapper'>
+          <h2 className='secondary-title'>Email</h2>
           <input
           className='input-item'
           type='email'
@@ -81,9 +136,16 @@ const showPwdConfirm = () => {
           onChange={handleEmail}
           required
           />
+          {
+            isEmailValid
+             ? 
+             null :
+             <span className='error-descr'>Wrong email</span> 
+          }
           </div>
         
           <div className='input-wrapper'>
+          <h2 className='secondary-title'>Password</h2>
           <input
           className='input-item'
            type={typePwd}
@@ -97,10 +159,12 @@ const showPwdConfirm = () => {
           src={inputIcon}
           alt='input-icon'
           onClick={showPwd}
+          className='input-icon'
           />
           </div>
           
           <div className='input-wrapper'>
+          <h2 className='secondary-title'>Confirm password</h2>
           <input
           className='input-item'
            type={typePwdConfirm}
@@ -114,12 +178,16 @@ const showPwdConfirm = () => {
           src={inputIcon}
           alt='input-icon'
           onClick={showPwdConfirm}
+          className='input-icon'
           />
           </div>
          
         </div>
 
-        <button className='sign-up-btn'>
+        <button className={`${isEmailValid 
+        && isPwdValid 
+        && isPwdConfirmValid ?
+        'pushed' : ''} sign-up-btn`} onClick={showAlert}>
          Sign Up
         </button>
         <div className='form-footer'>
